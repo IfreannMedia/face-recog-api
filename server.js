9cd9e7926098a0f1ccb9eeff84270bc88d83c7f9
Wario <37343942+IfreannMedia@users.noreply.github.com>
2021-02-06 15:10:03 +0100

    updated endpoints for register and sign in
    and get user by id and increment entries, using the knex and bcrypt libraries

--- server.js
+++ server.js
@@ -1,88 +1,117 @@
 const express = require('express');
-
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-const mockDb = {
-    users: [
-        {
-            id: '123',
-            name: 'smamcky',
-            email: 'smamkyJones@johnjo.com',
-            password: 'abdc1234',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '456',
-            name: 'sally',
-            email: 'sallySo@johnjo.com',
-            password: 'efgh4567',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-}
+const knex = require('knex');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'admin',
+        database: 'face-recog'
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('got the root');
 });
 
 app.get('/profile/:id', (req, res) => {
-    const {id} = req.params;
-    let found = false;
-    mockDb.users.forEach((user)=>{
-        if (user.id == id) {
-            found = true;
-            return res.json(user);
-        }
+    const { id } = req.params;
+    db.select('*').from('users').where({ id }).then((users) => {
+        res.status(users.length ? 200 : 400).json(users.length ? users[0] : `user with id ${id} not found`);
+    }).catch(err => {
+        console.error(new Error(err));
+        res.status(400).json(err);
     })
-    if(!found) {
-        res.status(404).json('no user found');
-    }
 });
 
 
 app.post('/signin', (req, res) => {
-    if (req.body.email === mockDb.users[0].email &&
-        req.body.password === mockDb.users[0].password) {
-        res.json('success');
-    } else {
-        res.status(400).json('invalid credentials');
-    }
+    db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(users => {
+        console.log(users);
+        if(users.length && compareHash(req.body.password, users[0].hash)) {   
+            return db.select('*').from('users')
+            .where('email' , '=', req.body.email)
+            .then(users => {
+                if(users.length){
+                    res.json(users[0]);
+                } else {
+                    console.error(new Error('user is in login and PW correct, but user not in users table'))
+                    return Promise.reject('unable to sign in, credentials invalid');
+                }
+            })
+            .catch(err => {
+                console.error(new Error(err));
+                return Promise.reject('unable to sign in, credentials invalid');
+            })
+        } else {
+            return Promise.reject('Credentials do not match our records');
+        }
+    }).catch(err => {
+        console.error(new Error(err));
+        res.status(400).json('unable to sign in, credentials invalid');
+    })
 });
 
 app.post('/register', (req, res) => {
-    const {email, name, password} = req.body;
-    mockDb.users.push(
-        {
-            id: '500',
-            name,
+    const { email, name, password } = req.body;
+    const hash = getEncryptedPassword(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash,
             email,
-            password,
-            entries: 0,
-            joined: new Date()
-        }
-    )
-    res.json(mockDb.users[mockDb.users.length -1]);
+        })
+            .into('login')
+            .returning('email')
+            .then((loginEmail) => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        email: loginEmail[0],
+                        name,
+                        joined: new Date()
+                    }).then(response => {
+                        res.json(response[0]);
+                    })
+            }).then(trx.commit).catch((err) => {
+                trx.rollback;
+                return Promise.reject(err)
+            }).catch(err => {
+                console.error(new Error(err));
+                res.status(400).json('unable to join');
+            });
+    })
 });
 
-app.put('/image', (req, res)=> {
-    const {id} = req.body;
-    let found = false;
-    mockDb.users.forEach((user)=>{
-        if (user.id == id) {
-            found = true;
-            user.entries++;
-        
-            return res.json(user.entries);
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id).increment('entries', 1).returning('entries').then((entries) => {
+        if (entries.length) {
+            res.status(200).json(entries[0]);
+        } else {
+            res.status(400).json('unable to increment entries');
         }
-    })
-    if(!found) {
-        res.status(404).json('no user found');
-    }
+    }).catch(err => res.status(400).json('could not update entires'));
 })
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+const port = 3001;
+app.listen(port, () => {
+    console.log(`app is running on port: ${port}`);
 });
+
+getEncryptedPassword = (plainTextPassword) => {
+    const salt = 10;
+    return bcrypt.hashSync(plainTextPassword, salt);
+}
+
+compareHash = (password, hash) => {
+    return bcrypt.compareSync(password, hash);
+}
