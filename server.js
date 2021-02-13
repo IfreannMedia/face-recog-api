9c744ad7313b28e89c2a11a1d68a1e80c3a83cdf
Wario <37343942+IfreannMedia@users.noreply.github.com>
2021-02-13 14:21:01 +0100

    added the postgres addon and url

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-spherical-89425',
         user: 'postgres',
         password: 'admin',
         database: 'face-recog'
